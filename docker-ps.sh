329b6d5ec68846d603bf38b7949837a0cfe7fc32
jadencamelot <10377013+jadencamelot@users.noreply.github.com>
2025-04-03 15:14:40 +0000

    Add colours to docker-ps.sh

--- docker-ps.sh
+++ docker-ps.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-sudo docker ps --format "table {{.ID}}\t{{.Names}}\t{{.RunningFor}}\t{{.Status}}\t{{.Image}}" | { read -r header; echo "$header"; sort -k2; }
+GREEN="\033[1;32m"  # Bold green
+BLUE="\033[0;34m"   # Regular blue (not bold)
+RESET="\033[0m"
+
+sudo docker ps --format "table {{.ID}}\t{{.Names}}\t{{.RunningFor}}\t{{.Status}}\t{{.Image}}" | {
+    read -r header
+    echo -e "${GREEN}$header${RESET}"
+    sort -k2 | while IFS= read -r line; do
+        echo -e "${BLUE}$line${RESET}"
+    done
+}
